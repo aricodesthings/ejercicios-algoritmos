23eb8cb5606c231dfe81fc98cf7fa0f98fb8f2a8
aricodesthings <ariguerrios@gmail.com>
2025-11-05 17:13:49 -0500

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,4 @@
+import random
+num = random.randint(0, 200)
+print("Número generado:", num)
+print("Aumentado en 30%:", num * 1.3)
---
+++ 8.py
@@ -0,0 +1,5 @@
+A = input("Ingrese la palabra A: ")
+B = input("Ingrese la palabra B: ")
+A, B = B, A
+print("Ahora A es:", A)
+print("Ahora B es:", B)
